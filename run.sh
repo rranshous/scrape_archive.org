cc5f50813da56322cb2b7ddbe972aba6b8fb37b1
Robby Ranshous <rranshous@gmail.com>
2014-11-29 17:03:47 -0500

    fix cd to dir w/ spaces, running w/ name

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -v /host/out/:/out/ slag.local:4567/scrape_archive.org
+docker run -v /gut/scrapes/archive.org/:/out/ --name scrape_archive.org -d slag.local:4567/scrape_archive.org
--- search_results.sh
+++ search_results.sh
@@ -2,7 +2,7 @@ CATEGORY=$1
 OUTDIR=$2
 SEARCH_URL="https://archive.org/search.php?query=subject:\"$CATEGORY\" AND mediatype:movies"
 echo "OUTDIR: $OUTDIR"
-cd $OUTDIR
+cd "$OUTDIR"
 for i in $(seq 1 20);
 do
   echo "$CATEGORY PAGE $i"
@@ -15,5 +15,5 @@ do
      grep "mp4" | \
      cut -d'"' -f4 | \
      grep "archive.org" | \
-     xargs -I{} wget -nv -nc {}
+     xargs -I{} wget -nv -nc {} 2>&1
 done
